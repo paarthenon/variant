3ca8667e15385a77d1b58369202d623a0ffa640b
Paarth <paarthk+gh@gmail.com>
2020-10-19 12:13:38 -0400

    attempt 3

---
+++ docs/ce858a4d.bb8ed4fc.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[26],{162:function(a){a.exports=JSON.parse('{"allTagsPath":"/variant/blog/tags","slug":"docusaurus","name":"docusaurus","count":1,"permalink":"/variant/blog/tags/docusaurus"}')}}]);
---
+++ docs/d1e57894.42385647.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[27],{163:function(a){a.exports=JSON.parse('{"intro":{"allTagsPath":"/variant/blog/tags","slug":"intro","name":"intro","count":1,"permalink":"/variant/blog/tags/intro"},"docusaurus":{"allTagsPath":"/variant/blog/tags","slug":"docusaurus","name":"docusaurus","count":1,"permalink":"/variant/blog/tags/docusaurus"}}')}}]);
